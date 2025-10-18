1665fbd03281fb21b4f6516cd72be064b62deafc
ga111o <ga111o@proton.me>
2025-10-18 10:31:55 +0900

    Add devServer configuration for local development

--- sowon-mirae-science-festival-choose-real-img/vue.config.js
+++ sowon-mirae-science-festival-choose-real-img/vue.config.js
@@ -4,4 +4,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
     process.env.NODE_ENV === "production" ? "/sowon-choose-img/" : "/",
+  devServer: {
+    host: true,
+    allowedHosts: ['festival2.ga111o.com']
+  }
 });
